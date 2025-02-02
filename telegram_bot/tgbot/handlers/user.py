63f6d75868f348486b2723ac9a9cbf6f5d7a51ac
ReiVanSTR <reivanstr@gmai.com>
2025-02-02 21:26:21 +0100

    announcement update

--- telegram_bot/tgbot/handlers/user.py
+++ telegram_bot/tgbot/handlers/user.py
@@ -10,6 +10,11 @@ async def user_start(message: Message):
     await message.reply(f"Hello {message.from_user.username}")
     
 @user_router.message(Command("announce"))
-async def announce(message: Message, tga_logger):
+async def announce(message: Message, broadcaster):
     await message.answer(f"Processing")
-    await tga_logger(message.from_user.id, "Test announcement")
+    await broadcaster(message.from_user.id, "Test announcement")
+    
+@user_router.message(Command("menu"))
+async def menu(message: Message, broadcaster):
+    await message.answer(f"Processing")
+    await broadcaster(message.from_user.id, "Test announcement")
