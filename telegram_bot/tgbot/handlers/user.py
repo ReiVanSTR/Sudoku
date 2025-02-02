4a6a7af65eb578d6941410cfa0d4c84212b0d903
ReiVanSTR <reivanstr@gmai.com>
2025-02-02 21:29:05 +0100

    test

--- telegram_bot/tgbot/handlers/user.py
+++ telegram_bot/tgbot/handlers/user.py
@@ -12,9 +12,9 @@ async def user_start(message: Message):
 @user_router.message(Command("announce"))
 async def announce(message: Message, broadcaster):
     await message.answer(f"Processing")
-    await broadcaster(message.from_user.id, "Test announcement")
+    await broadcaster("Test announcement \nuptime: 100% \n Active proxies: 5 \n Active api keys: 39")
     
 @user_router.message(Command("menu"))
 async def menu(message: Message, broadcaster):
     await message.answer(f"Processing")
-    await broadcaster(message.from_user.id, "Test announcement")
+    await broadcaster("Test announcement")
